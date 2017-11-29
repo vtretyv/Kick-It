e90c508952bd0714d8153e0f5d6843e0402c267f
ngratzi <ngratzi@g.clemson.edu>
2017-11-29 12:14:27 -0800

    Pie chart component created and hooked up to state

---
+++ client/src/components/Piechart.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { scaleOrdinal } from 'd3-scale';
+import { arc, pie } from 'd3-shape';
+
+const color = scaleOrdinal(['#98abc5', '#8a89a6', '#7b6888', '#6b486b', '#a05d56', '#d0743c', '#ff8c00']);
+
+const width = 400;
+const height = 400;
+const radius = Math.min(width, height) / 2;
+
+
+const dataArc = arc()
+  .outerRadius(radius - 10)
+  .innerRadius(0);
+
+const labelArc = arc()
+    .outerRadius(radius - 40)
+    .innerRadius(radius - 40);
+
+const pieChart = pie()
+    .sort(null)
+    .value(d => d.value);
+
+
+const Piechart = ({ data }) => (
+  <svg className="Piechart" width={width} heigth={height} >
+    <g transform={`translate(${width / 2}, ${height / 2})`}>
+      {pieChart(data).map((d, i) => (
+        <g key={i} className="arc">
+          <path
+            d={dataArc(d)}
+            fill={color(d.data.label)}
+          />
+          <text
+            dy='.55em'
+            transform={`translate(${labelArc.centroid(d)})`}
+          >
+            {d.data.label}
+          </text>
+        </g>
+      ))}
+    </g>
+  </svg>
+);
+
+export default Piechart;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBarContainer from './components/SearchBarContainer.jsx';
 import EventListContainer from './components/EventListContainer.jsx';
-// import WeekendListContainer from './components/WeekendListContainer.jsx';
+import Piechart from './components/Piechart.jsx';
+
+
+ // 103   | music         | Music
+ // 110   | food          | Food & Drink
+ // 113   | community     | Community Events
+ // 116   | community     | Community Events
+ // 17001 | dating        | Dating
+ // 104   | entertainment | Entertainment
+ // 105   | entertainment | Entertainment
+ // 102   | science       | Science/Tech
+ // 118   | autoBoatAir   | Auto, Boat, Air
+ // 108   | active        | Active
+ // 109   | active        | Active
 
 class App extends React.Component {
   constructor() {
@@ -102,13 +115,94 @@ class App extends React.Component {
         <SearchBarContainer runFilters={this.runFilters.bind(this)}/>
         <div className="album text-muted">
           <div className="container">
+=======
+      d3Data: []
+    }
+  }
+  componentDidMount() {
+    fetch('/initialLoad')
+      .then((response) => {
+        console.log('response received from server:', JSON.stringify(response));
+        return response.json();
+      })
+      .then((data) => {
+        console.log('data about to be put into todays state:', data.today);
+
+        // creates massaged data for d3
+        const d3Data = this.createD3Data(data.today);
+
+        // Passes d3data into state
+        this.setState({
+          featured: data.today,
+          d3Data: d3Data
+        })
+      })
+      .then(() => {
+        fetch('/weekend')
+          .then((response) => {
+            console.log('data from API for weekend', response);
+            return response.json();
+          })
+          .then((data) => {
+            //console.log('data about to be put into weekend state: ' , data);
+            let events = JSON.parse(data).events;
+            this.setState({
+              weekend: events,
+            });
+          });
+      });
+  }
+
+  createD3Data(raw) {
+
+    const d3Data = [
+      { label: 'music', value: 33 },
+      { label: 'food', value: 53 },
+      { label: 'community', value: 32 },
+      { label: 'dating', value: 21 },
+      { label: 'entertainment', value: 13 },
+      { label: 'science', value: 14 },
+      { label: 'autoBoatAir', value: 14 }
+    ];
+
+    return d3Data;
+  }
+
+  runFilters(filters) {
+    fetch('/filter', {
+      headers: {
+        //'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(filters),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((events) => {
+        this.setState({
+          featured: events.rows
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Kick It</h1>
+        <SearchBarContainer runFilters={this.runFilters.bind(this)}/>
+        <div className="album text-muted">
+          <div className="container">
+
+            <Piechart data={this.state.d3Data} />
+
             <EventListContainer 
               featuredEvents={this.state.featured}
               weekendEvents={this.state.weekend.slice(0,10)} 
             />
           </div>
-        </div>
-        
+        </div>        
       </div>
     )
   }
