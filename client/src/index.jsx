3a81ffeec2c829dc2fa180454fe6e4d460ebb765
Bernard Kitchens <bernardkitchens@Bernards-MacBook-Pro.local>
2017-11-30 10:23:17 -0800

    commented out eventmap component

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -187,12 +187,14 @@ class App extends React.Component {
 							weekendEvents={this.state.weekend.slice(0,10)} 
 						/>
 					</div>
+					{/* <div>
 					<EventMap
             googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyC4R6AN7SmujjPUIGKdyao2Kqitzr1kiRg&v=3.exp&libraries=geometry,drawing,places"
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `350px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
           />
+					</div> */}
 				</div>
 				
 			</div>
