6fb9ed9be12a9495e261966a34721bca1ebbc9cf
ngratzi <ngratzi@g.clemson.edu>
2017-11-30 11:20:00 -0800

    rm config

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  APIKEY: 'Bearer 5IBV7MPE324H46CPUATD',
-  GOOGLE_CLIENT_ID: '806104869803-mlenko8a13ikagk5qjen3n72uiqv9fvd.apps.googleusercontent.com',
-  GOOGLE_CLIENT_SECRET: '5U1TNK4Tc-DrSak-fZtJ87YG',
-};
