553ebff68aa77771dcaea41f72fe596aecafe9cb
Vladimir Tretyakov <vladimirtretyakov226@gmail.com>
2017-11-30 15:12:37 -0800

    Fished out the duplicate error finally, dates rendering properly for new cities, WOOOO

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -134,9 +134,11 @@ class App extends React.Component {
 				this.setState({
 					featured: events.today.rows
 				})
-			}
-			
+			}	
 		})
+		// .then(() => {
+		// 	fetch(`/weekend/${}`)
+		// })
 	}
 
   render() {
