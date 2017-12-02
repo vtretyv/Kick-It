7e770b51498c46e62345127f011e2923c5645e92
Bernard Kitchens <bernardkitchens@Bernards-MacBook-Pro.local>
2017-12-01 19:02:36 -0800

    commented out sample data

--- client/src/components/EventMap.jsx
+++ client/src/components/EventMap.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
 
-const { GOOGLE_API_KEY } = require('../../../config.js');
-const geocoder = require('google-geocoder');
+// const { GOOGLE_API_KEY } = require('../../../config.js');
+// const geocoder = require('google-geocoder');
 
 
 const EventMap = withScriptjs(withGoogleMap(props =>
@@ -24,29 +24,29 @@ export default EventMap;
 
 // testing geocode
 
-const geo = geocoder({
-  key: GOOGLE_API_KEY,
-});
+// const geo = geocoder({
+//   key: GOOGLE_API_KEY,
+// });
 
-// sample address data
-const places = ['944 Market Street, San Francisco', '201 Folsom Street, San Francisco', '245 Market Street, San Francisco',
-  '2500 Market Street, San Francisco', '4288 24th Street San Francisco', '101 Montgomery Street San Francisco',
-  '563 Ruger Street, San Francisco', '2114 Filmore Street, San Francisco', '55 Music Concourse Drive, San Francisco',
-  '824 Antoinette Lane, South San Francisco'];
+// // sample address data
+// const places = ['944 Market Street, San Francisco', '201 Folsom Street, San Francisco', '245 Market Street, San Francisco',
+//   '2500 Market Street, San Francisco', '4288 24th Street San Francisco', '101 Montgomery Street San Francisco',
+//   '563 Ruger Street, San Francisco', '2114 Filmore Street, San Francisco', '55 Music Concourse Drive, San Francisco',
+//   '824 Antoinette Lane, South San Francisco'];
 
-// function retrieves lat/long of each address
-const getLatLong = (arr) => {
-  const locations = [];
-  arr.forEach((address) => {
-    geo.find(address, (err, res) => {
-      locations.push(res[0].location);
-    });
-  });
-  console.log(locations);
-  return locations;
-};
+// // function retrieves lat/long of each address
+// const getLatLong = (arr) => {
+//   const locations = [];
+//   arr.forEach((address) => {
+//     geo.find(address, (err, res) => {
+//       locations.push(res[0].location);
+//     });
+//   });
+//   console.log(locations);
+//   return locations;
+// };
 
-const savedLocations = getLatLong(places);
+// const savedLocations = getLatLong(places);
 
 // get locations from API
 // https://www.eventbriteapi.com/v3/venues/15304516/?token={API_KEY}
