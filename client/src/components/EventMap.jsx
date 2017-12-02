ff0f5d1bf38489c1eb321f57b053f996092f296c
Bernard Kitchens <bernardkitchens@Bernards-MacBook-Pro.local>
2017-12-02 14:13:04 -0800

    city search now updates, centers map

--- client/src/components/EventMap.jsx
+++ client/src/components/EventMap.jsx
@@ -7,47 +7,16 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-map
 
 const EventMap = withScriptjs(withGoogleMap(props =>
   (<GoogleMap
-    defaultZoom={12}
+    defaultZoom={10}
     center={props.defaultLocation}
   >
     {props.venues.map((address, index) => {
-      return <Marker
-               
-               key={index.toString()}
-               position={address}
-             />
+      return (<Marker
+        key={index.toString()}
+        position={address}
+      />);
 })}
-  </GoogleMap>)));
+   </GoogleMap>)));
 
 export default EventMap;
 
-
-
-// testing geocode
-
-// const geo = geocoder({
-//   key: GOOGLE_API_KEY,
-// });
-
-// // sample address data
-// const places = ['944 Market Street, San Francisco', '201 Folsom Street, San Francisco', '245 Market Street, San Francisco',
-//   '2500 Market Street, San Francisco', '4288 24th Street San Francisco', '101 Montgomery Street San Francisco',
-//   '563 Ruger Street, San Francisco', '2114 Filmore Street, San Francisco', '55 Music Concourse Drive, San Francisco',
-//   '824 Antoinette Lane, South San Francisco'];
-
-// // function retrieves lat/long of each address
-// const getLatLong = (arr) => {
-//   const locations = [];
-//   arr.forEach((address) => {
-//     geo.find(address, (err, res) => {
-//       locations.push(res[0].location);
-//     });
-//   });
-//   console.log(locations);
-//   return locations;
-// };
-
-// const savedLocations = getLatLong(places);
-
-// get locations from API
-// https://www.eventbriteapi.com/v3/venues/15304516/?token={API_KEY}
